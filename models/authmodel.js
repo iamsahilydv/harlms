65a5806873239d3b7e6d4b585afa369b4308c46e
ingole8888 <ingole8888@gmail.com>
2023-03-12 16:32:47 +0530

    data added

--- models/authmodel.js
+++ models/authmodel.js
@@ -1,11 +1,21 @@
 const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
+    fname:{type:String,required : true },
+    lname:{type:String,required : true },
+    mobile:{type:Number, required:true},
+    alternatemobile:{type:Number, required:true},
+    policerange:{type:String},
+    districtofc:{type:String,required:true},
+    spname:{type:String},
+    designation:{type:String},
+    policestation:{type:String},
     email : {type : String, required : true},
-    password : {type : String, required : true}
+    password : {type : String, required : true},
+    role:{type:String}
 })
 
-const AuthModel = mongoose.model("user", userSchema)
+const AuthModel = mongoose.model("myuserdata", userSchema)
 
 
 module.exports = AuthModel
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -4,23 +4,45 @@ const bcrypt  = require("bcrypt")
 const jwt = require('jsonwebtoken');
 
 const AuthModel =require("../models/authmodel")
+
 const authController = express.Router();
 
+//for all users creation
 authController.post("/signup",async (req, res) => {
-    const {email, password} = req.body;
+    const {email, password,fname,lname,mobile,alternatemobile,policerange,
+        districtofc,spname,designation,policestation,role} = req.body;
 
-    await bcrypt.hash(password, 6, async function(err, hash) {
-        if(err){
-            return res.status(500).send("Password Error, Please try again...")
-        }
-        const user = new AuthModel({
-            email,
-            password : hash
-        })
-        await user.save();
-         return res.status(200).send({ message: "Signup successfull", user: user})
-    });
+        const user = await AuthModel.findOne({email})
+  
+    if(user){
+        return res.status(501).send("User already present please use different userid!");
+    }
+    
+        await bcrypt.hash(password, 6, async function(err, hash) {
+            if(err){
+                return res.status(500).send("Password Error, Please try again...")
+            }
+            const user = new AuthModel({
+                email,
+                password : hash,
+                fname,
+                lname,
+                mobile,
+                alternatemobile,
+                policestation,
+                policerange,
+                districtofc,
+                spname,
+                designation,
+                role
+            })
+            await user.save();
+             return res.status(200).send({ message: "Signup successfull", user: user})
+        });
+ 
 })
+
+
 
 authController.post("/login", async (req, res) => {
     const {email, password} = req.body;
@@ -45,6 +67,37 @@ authController.post("/login", async (req, res) => {
         res.status(401).send("invalid password")
     }
     });
+})
+
+
+//get all sp
+authController.get("/allsp", async (req,res)=>{
+    const user = await AuthModel.find({designation:"SP"})
+    res.send(user)
+})
+
+//for all dsp
+authController.get("/alldsp", async (req,res)=>{
+    const user = await AuthModel.find({designation:"DSP"})
+    res.send(user)
+})
+
+//for all asp
+authController.get("/allasp", async (req,res)=>{
+    const user = await AuthModel.find({designation:"ASP"})
+    res.send(user)
+})
+
+//for all dsp
+authController.get("/allsho", async (req,res)=>{
+    const user = await AuthModel.find({designation:"SHO"})
+    res.send(user)
+})
+
+//for all IO
+authController.get("/allio", async (req,res)=>{
+    const user = await AuthModel.find({designation:"IO"})
+    res.send(user)
 })
 
 module.exports = authController
