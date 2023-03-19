a3f57dc4cf7c53ffa75bfab814e947c905ba6f6b
Sahil <100nukhola123@gmail.com>
2023-03-20 00:47:41 +0530

    Update policestationmodel.js

--- models/policestationmodel.js
+++ models/policestationmodel.js
@@ -3,7 +3,8 @@ const mongoose=require("mongoose")
 const policeStationSchema= new mongoose.Schema({
     policeStationName:{type:String},
     phoneNumber:{type:Number},
-    emailAddress :{type:String}
+    policeStationDistrict :{type:String},
+    policeStationRange:{type:String}
 })
 
 const policeStationModel=mongoose.model("policeStation",policeStationSchema)
